51701dbac6adad49b0238fc3bf2bb5fa6b5fa19f
zjhzxjm <xujm@realbio.cn>
2016-12-30 22:55:52 +0800

    修正客户管理权限控制

--- crm/admin.py
+++ crm/admin.py
@@ -7,5 +7,15 @@ class CustomerAdmin(admin.ModelAdmin):
     Admin class for customer
     """
     list_display = ('name', 'organization', 'email', 'phone', 'call', 'address')
+    ordering = ['name']
+    list_filter = ['organization']
+    search_fields = ['name', 'organization', 'phone', 'call']
+
+    def get_queryset(self, request):
+        # 只允许管理员和拥有该模型删除权限的人员才能查看所有样品
+        qs = super(CustomerAdmin, self).get_queryset(request)
+        if request.user.is_superuser or request.user.has_perm('crm.delete_customer'):
+            return qs
+        return qs.filter(linker=request.user)
 
 admin.site.register(Customer, CustomerAdmin)
