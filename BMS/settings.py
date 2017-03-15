e7f41767c8f3d7c503640b65c7d08453c05a6f1d
zjhzxjm <xujm@realbio.cn>
2017-03-15 13:35:22 +0800

    正式测试前数据库变换防覆盖

--- BMS/settings.py
+++ BMS/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'BMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'dev.db'),
     }
 }
 
